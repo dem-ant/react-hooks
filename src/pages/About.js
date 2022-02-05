6f012e39daee5ab0fdf61c174a8b460d60cc4b41
unknown <demchenko_a@ekb.sima-land.ru>
2022-02-05 12:50:02 +0500

    react-hooks

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,11 +2,9 @@ import React from 'react'
 
 export const About = () => {
   return (
-    <div className='jumbotron jumbotron-fluid'>
-      <div className='container'>
+      <div className='container-fluid bg-light p-5'>
         <h1 className='display-4'>Информация</h1>
         <p className='lead'>Версия приложения: <strong>1.0.0</strong></p>
       </div>
-    </div>
   )
 }
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,79 @@
-import React from 'react'
+import React, { Fragment, useContext, useEffect } from 'react'
+import {Link, useParams} from 'react-router-dom'
+import { Repos } from '../components/Repos'
+import { GithubContext } from '../context/github/githubContext'
 
 export const Profile = () => {
+  const {getUser, getRepos, loading, user, repos} = useContext(GithubContext)
+  const params = useParams()
+  const urlName = params.name
+
+  useEffect(() => {
+    getUser(urlName)
+    getRepos(urlName)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  if (loading) {
+    return <p className='text-center'>Loading...</p>
+  }
+
+  const {
+    name, company, avatar_url, location, bio, blog, following,
+    login, html_url, followers, public_repos, public_gists
+  } = user
+
   return (
-    <div>
-      <h1>Profile page</h1>
-    </div>
+    <Fragment>
+      <Link to='/' className='btn btn-link'>На главную</Link>
+
+      <div className='card mb-4'>
+        <div className='card-body'>
+          <div className='row'>
+            <div className='col-sm-3 text-center'>
+              <img
+              src={avatar_url}
+              alt={name}
+              style={{width: '150px'}} />
+              <h1>{name}</h1>
+              {location && <p>Местоположение: {location}</p>}
+            </div>
+            <div className='col'>
+              {
+                bio && <Fragment>
+                  <h3>BIO</h3>
+                  <p>{bio}</p>
+                </Fragment>
+              }
+              <a
+                href={html_url}
+                target='_blank'
+                rel='noreferrer'
+                className='btn btn-dark'
+              >
+                Открыть профиль
+              </a>
+              <ul>
+                {login && <li>
+                  <strong>Username: </strong> {login}
+                  </li>}
+                {company && <li>
+                  <strong>Company: </strong> {company}
+                  </li>}
+                {blog && <li>
+                  <strong>Website: </strong> {blog}
+                  </li>}
+              </ul>
+
+              <div className='badge bg-primary'>Подписчики: {followers}</div>
+              <div className='badge bg-success'>Подписан: {following}</div>
+              <div className='badge bg-info'>Репозитории: {public_repos}</div>
+              <div className='badge bg-dark'>Gists: {public_gists}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+       <Repos repos={repos} />
+    </Fragment>
   )
 }
